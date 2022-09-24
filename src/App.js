555968b3e3804eeec43b279333e2f9e8bbc94179
Rasel <mdraselm413@gmail.com>
2022-09-24 22:34:03 +0600

    recap-code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <District name = 'NoaKhali' special = 'vibag'></District>
+      <District name = 'NoaKhali' special = 'Bivag'></District>
       <District name ='Brammonbariya' special = 'Real Hero'></District>
       <District name = 'Kumilla' special = 'Moynamoti'></District>
     </div>
@@ -12,15 +13,23 @@ function App() {
 }
 
 const districtStyle = {
-  backgroundColor: 'lightBlue', margin: '20px', borderRadius: '20px'
+  backgroundColor: 'lightBlue', margin: '20px', borderRadius: '20px',padding: '20px'
 }
 
 function District(props){
+  const [power,setPower] = useState(1)
+  const bootsPower = () => {
+    const newPower = power * 2;
+    setPower(newPower)
+  }
   return(
     <div style={districtStyle}>
       <h2>Name:{props.name} </h2>
       <p>Speciality:{props.special} </p>
+      <h4>Power:{power}</h4>
+      <button onClick={bootsPower}>Boots The Power</button>
     </div>
+    
 
   )
 }
