6ee83d31a115e6eeb00ac0aaa45135f1ef85595e
Rasel <mdraselm413@gmail.com>
2022-09-24 23:47:14 +0600

    post added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <LoadPosts></LoadPosts>
       <District name = 'NoaKhali' special = 'Bivag'></District>
       <District name ='Brammonbariya' special = 'Real Hero'></District>
       <District name = 'Kumilla' special = 'Moynamoti'></District>
     </div>
   );
+}
+
+function LoadPosts(){
+  const [posts, setPosts] = useState([]);
+  useEffect( () =>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setPosts(data))
+  },[])
+  return(
+    <div>
+      <h1>Total Posts:{posts.length}</h1>
+      {
+        // posts.map( post => console.log(post))
+        posts.map( post => <Post title ={post.title} body ={post.body}></Post>)
+      }
+    </div>
+  )
+}
+
+function Post(props){
+  return(
+    <div style={{backgroundColor:'lightsalmon', margin: '20px', padding:'15px', border: '2px solid Blue'}}>
+      <h2>Title: {props.title}</h2>
+      <p>Body: {props.body}</p>
+    </div>
+  )
 }
 
 const districtStyle = {
